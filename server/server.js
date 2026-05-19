395b9e93b468b605f8a5c74a5009d76fb8e03128
khojodelly <abugribismark4@gmail.com>
2026-05-19 17:16:15 +0100

    Fix Gmail Issues

--- server/server.js
+++ server/server.js
@@ -751,14 +751,11 @@ function authenticateToken(
 }
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    connectionTimeout: 10000
+    }
 });
 
 transporter.verify((err, success) => {
